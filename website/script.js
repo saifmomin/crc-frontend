c4c9caff57b7ae8b438907be43cca336855b2071
saifmomin <saifmomin21@gmail.com>
2021-07-24 16:12:28 +0530

    Initial commit

--- website/script.js
+++ website/script.js
@@ -1,61 +1,33 @@
-//window.onload = fetchCount;
-
+//Main Script
 window.addEventListener("load", myInit, true); function myInit(){
 fetchCount();
 todaysDate();
  };
 
 function setInterval(){
-var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout(todaysDate,refresh)
+    var refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout(todaysDate,refresh)
 }
 
 function todaysDate() {
-var now = new Date();
-//const nowLocale = now.toLocaleString('en-US');
-const nowUTC = now.toUTCString();
-//document.getElementById('date').innerHTML = "Today is " +   nowUTC ;
-document.getElementById('date').innerHTML = nowUTC ;
-setInterval();
-//console.log("Today: " +now);
+    var now = new Date();
+    const nowUTC = now.toUTCString();
+    document.getElementById('date').innerHTML = nowUTC ;
+    setInterval();
  }
 
-
-
-  //alert("starting main script");
- console.log("starting main script");
+console.log("starting main script");
 
 function fetchCount() {
-
-  //alert("inside fetchcount");
-
-const worker = new Worker("worker.js");
-
-  //alert("worker initialized");
-
-worker.onmessage = function(event) {
-
- // alert("Got: " + event.data);
- console.log("Received result back from worker.js: " + event.data);
-
-// document.getElementById("visitor_count").innerHTML = "You're guest # " +   event.data + " to check out my profile page !";
-document.getElementById("visitor_count").innerHTML = event.data;
-
- }
-
- // alert(" posting msg to worker");
- console.log("posting msg to worker.js");
-
-apiUri='https://7l2enkiaj0.execute-api.ap-south-1.amazonaws.com/Stage/visits';
-worker.postMessage(apiUri);
-
-
-//alert("terminating web worker from main script");
-// immediately terminate the main JS file
-//worker.terminate();
+    const worker = new Worker("worker.js");
+    worker.onmessage = function(event) {
+        console.log("Received result back from worker.js: " + event.data);
+        document.getElementById("visitor_count").innerHTML = event.data;
+    }
+    console.log("posting msg to worker.js");
+    apiUri='https://7l2enkiaj0.execute-api.ap-south-1.amazonaws.com/Prod/visits';
+    worker.postMessage(apiUri);
 }
-
-
 
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
--- website/worker.js
+++ website/worker.js
@@ -1,20 +1,11 @@
-﻿
-onmessage=function(event){  
-
-console.log("Got msg inside worker.js: " +event.data);
-
-      fetch(event.data).then(function(response) {
-           					 return response.json();
-       				          }).then(function(json) {
-           							 return json;
-       						           }).then(function(result) {
-     									 // console.log(result);
-       									//alert(result)
-									postMessage(result);
-  									  });
-
-// stop the worker from the worker code.
-//self.close();
-
+﻿//Worker Script
+onmessage=function(event){
+    fetch(event.data).then(function(response) {
+           					return response.json();
+       				        }).then(function(json) {
+           					return json;
+       						}).then(function(result) {
+							postMessage(result);
+  							});
 }
 
